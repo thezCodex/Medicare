3df10fca9a54e64751a156be3a2905425d366627
easycode07 <akinwumitolulope274@gmail.com>
2024-07-24 22:30:07 +0100

    first working

---
+++ index.js
@@ -0,0 +1,21 @@
+// let currentIndex = 0;
+// const items = document.querySelectorAll('.background-container');
+// const totalItems = items.length;
+
+// function showItem(index) {
+//   items.forEach(item => {
+//     item.style.display = 'none';
+//   });
+//   items[index].style.display = 'inline-block';
+// }
+
+// function rotateItems() {
+//   showItem(currentIndex);
+//   currentIndex = (currentIndex + 1) % totalItems;
+// }
+
+
+// showItem(currentIndex);
+
+// // Rotating items every 3 seconds (adjust timing as needed)
+// setInterval(rotateItems, 3000);
